a72f5a3d42f5c45f739e35cdedaaa1cdd0a58f1b
MaradiSu <p.suresh@campusuvce.in>
2021-06-23 19:07:49 +0530

    size matters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,7 +81,7 @@ export default function Home() {
   function Iframe(props) {
     return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
   }
-  const iframe = '<iframe src="https://derekevans.herokuapp.com/jobsite" width="100%" height="500" allowtransparency="true" allowfullscreen="true"></iframe>'; 
+  const iframe = '<iframe src="https://derekevans.herokuapp.com/jobsite" width="100%" height="750" allowtransparency="true" allowfullscreen="true"></iframe>'; 
 
   return (
     <>
